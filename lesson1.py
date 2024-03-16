2dfb8504b7f929623da87f1873170c82e7162f6a
aleksandreCH <162612528+aleksandreCH@users.noreply.github.com>
2024-03-17 00:51:29 +0400

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,75 @@
+from turtle import *
+
+#we want to pain a house
+
+#step 1: draw a square
+speed(30)
+width(5)
+color("blue")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+#end of square
+
+#drawing a door
+
+forward(70)
+color("yellow")
+left(90)      
+forward(100)  #height of the door
+right(90)
+forward(60)
+right(90)
+forward(100)
+
+penup()
+goto(200,200)
+pendown()
+
+color("red")
+begin_fill()
+right(150)
+forward(200)     
+left(120)
+forward(200)
+end_fill()
+
+#draw window
+color("green")
+begin_fill()
+left(30)
+forward(70)
+left(90)
+forward(70)
+left(90)
+forward(70)
+left(90)
+forward(70)
+left(180)
+forward(200)
+right(90)
+forward(70)
+right(90)
+forward(70)
+right(90)
+forward(70)
+right(90)
+forward(70)
+end_fill()
+
+
+
+
+
+
+
+
+exitonclick()
---
+++ lesson2.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+print("aleksandre chogovadze") # it's me
+
+print("irina chogovadze") # it's my mom
+
+print("irakli chogovadze") # it's my ded
+
+print("ilia chogovadze") # it's my brother
+
+print("alexa") # it's my dog
+
+
+
+
+
+
+
+
+
