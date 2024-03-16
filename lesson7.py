276547bd938a31bdcf81b26a87c2213dbda0930f
aleksandreCH <162612528+aleksandreCH@users.noreply.github.com>
2024-03-17 00:53:52 +0400

    Add files via upload

---
+++ lesson7.py
@@ -0,0 +1,25 @@
+name = input('Enter your name: ')
+print(name)
+last_name = input('Enter your last name: ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson9.py
@@ -0,0 +1,22 @@
+math = int(input("enter your math greade: "))
+english = int(input("enter your english greade"))
+history = int(input("enter your history greade"))
+
+print(math+english+history/3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
